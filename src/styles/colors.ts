f06da20736f41b27051b6bed6d0d60d979588684
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-09-11 09:05:21 +0100

    [create]: creating  colors tokens

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export default {
+    background_navbar_primary:"#D2D2D2",
+    background_primary:"#D2D2D2",
+    active_primary_color:"#6000FC",
+    background_footer:"#4B4B4B",
+    text_card: "#656262",
+    phrase: "#2C2C2C",
+    background_section_body_primary: "#D2D2D2",
+    background_section_body_secondary:"#FFFFFF",
+}
