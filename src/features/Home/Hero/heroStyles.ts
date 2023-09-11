e8f22d42d9c7bf6c272d93bbfb7884d7302a8891
Estevao Sipitali <sipitali@MacBook-Pro-de-Nilton.local>
2023-09-11 13:52:10 +0100

    [add] news in section news in home page

--- src/features/Home/Hero/heroStyles.ts
+++ src/features/Home/Hero/heroStyles.ts
@@ -23,7 +23,6 @@ export const Section = styled.section`
   width: 100%;
   height: 100vh;
   overflow: hidden;
-
   & div {
     position: absolute;
     top: 0;
--- src/pages/Noticias/noticiasStyles.ts
+++ src/pages/Noticias/noticiasStyles.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-export const Main = styled.main`
+export const Container = styled.div`
   padding: 6rem 0;
+  display: flex;
 `;
-
 export const Div = styled.div`
   margin: auto;
   max-width: 1200px;
