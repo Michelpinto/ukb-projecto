790b2d3ae0f90e8e415b24a7c6aeecef02b80bb4
michelpinto <michabbe9@gmail.com>
2023-09-12 13:05:28 +0200

    [criado]: section de patrocinadores

--- src/features/Home/Noticias/noticias-styles.ts
+++ src/features/Home/Noticias/noticias-styles.ts
@@ -1,51 +1,49 @@
 import styled from 'styled-components';
 
-
 export const Container = styled.div`
   display: flex;
-  height:100%;
+  height: 100%;
   flex-direction: column;
-  max-width:1024px;
-  justify-content:center;`
+  max-width: 120rem;
+  justify-content: center;
+`;
 
-export const GridWrapper=styled.div`
+export const GridWrapper = styled.div`
   display: grid;
-  padding-left:1rem;
-  max-width:1024px;
-  grid-template-columns:repeat(2,auto);
-  grid-gap:12px;
-`
-export const NewsWrapper=styled.div`
+  padding-left: 1rem;
+  max-width: 120rem;
+  grid-template-columns: repeat(2, auto);
+  grid-gap: 1.2rem;
+`;
+export const NewsWrapper = styled.div`
   display: flex;
-  justify-content:center;
-  align-items:center;
-`
-export const NewsHeader=styled.div`
+  justify-content: center;
+  align-items: center;
+`;
+export const NewsHeader = styled.div`
   display: flex;
-  width:100%;
-  height:5rem;
-  flex-direction:row;
-  justify-content:space-between;
-  align-items:center;
-`
-export const PrimaryNewsWrapper=styled.div`
+  width: 100%;
+  height: 5rem;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const PrimaryNewsWrapper = styled.div`
   display: flex;
-  height:100%;
-  
-  flex-direction:column;
+  height: 100%;
+
+  flex-direction: column;
   justify-content: flex-end;
-  align-items:flex-end;
-  border-radius:6px;
-  background: ${props => props.theme.colors.background_primary}; 
-`
-export const SectionWrapper=styled.section`
-  display:flex;
+  align-items: flex-end;
+  border-radius: 0.6rem;
+  background: ${(props) => props.theme.colors.background_primary};
+`;
+export const SectionWrapper = styled.section`
+  display: flex;
   position: relative;
   width: 100%;
-  height: 670px;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  
-`
-
+  height: 67rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
