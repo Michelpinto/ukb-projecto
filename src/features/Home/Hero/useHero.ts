eeb8e29571f5cfb06b4f3e2a9e60ef0211c2f5a2
michelpinto <michabbe9@gmail.com>
2024-01-10 09:45:19 +0200

    adding real images

--- src/features/Home/Hero/useHero.ts
+++ src/features/Home/Hero/useHero.ts
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
-import landscape1 from '../../../assets/landscapeOne.jpg';
-import landscape2 from '../../../assets/landscapeTwo.jpg';
-import landscape3 from '../../../assets/landscapeThree.jpg';
+// import landscape1 from '../../../assets/landscapeOne.jpg';
+// import landscape2 from '../../../assets/landscapeTwo.jpg';
+// import landscape3 from '../../../assets/landscapeThree.jpg';
+import curso1 from '../../../assets/curso1.avif';
+import curso2 from '../../../assets/curso2.avif';
+import curso3 from '../../../assets/curso3.avif';
 
-const images = [landscape1, landscape2, landscape3];
+const images = [curso2, curso3, curso1];
 
 export const useHero = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
